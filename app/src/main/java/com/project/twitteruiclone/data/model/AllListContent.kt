0d1a062726792d3b3a9a0a92a68d9633eb181769
Nwaeme Olisemeka <35772751+nwaeme-olise@users.noreply.github.com>
2022-04-17 00:25:51 +0100

    Started implementation of RecyclerView classes for All Fragment

---
+++ app/src/main/java/com/project/twitteruiclone/data/model/AllListContent.kt
@@ -0,0 +1,9 @@
+package com.project.twitteruiclone.data.model
+
+data class AllListContent(
+    var action: String,
+    var userProfilePicture: Int,
+    var userName: String,
+    var actionText: String,
+    var actionContent: String
+)
